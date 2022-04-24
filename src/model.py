c3c87176542b0dd194c90bd2e6e0336323449ff9
Ahmed Badr <ahmed.k.badr.97@gmail.com>
2022-04-25 01:00:28 +0200

    create model in model.py

---
+++ src/model.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BikeSharingModel(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.input_size = input_size
+
+        self.fc1 = nn.Linear(input_size, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 1)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        x = self.fc3(x)
+        return x
